f9db80494b2c1266ca44223cf988a54e63696305
Soulmate404 <1440342414@qq.com>
2024-11-25 22:35:19 +0800

    源文件上传

---
+++ Book.c
@@ -0,0 +1,11 @@
+
+
+
+typedef struct Node {
+	int id;				//���
+	char name[50];		//����
+	char author[20];	//����
+	int inventory;		//�����
+	float price;		//�۸�
+	struct Node* next;	//ָ����
+} Book;	//�ṹ�嶨��ΪBook
